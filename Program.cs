d5055e1f44ca9970b8f8b19be67ef99921cd21da
MattiasNordenberg <mattias.nordenberg@gmail.com>
2021-01-19 11:36:31 +0100

    Set Up

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgift09_StenSaxPase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
